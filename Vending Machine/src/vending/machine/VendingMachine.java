9ba416935ee1bd4ecec4eef62f3727528c570d7a
comichero11 <richard.lam.0301@gmail.com>
2017-05-12 10:24:19 -0700

    Add files via upload

--- Vending Machine/src/vending/machine/VendingMachine.java
+++ Vending Machine/src/vending/machine/VendingMachine.java
@@ -7,7 +7,7 @@ package vending.machine;
 
 /**
  *
- * @author y.bulsara
+ * @author Richard Lam
  */
 public class VendingMachine {
 
@@ -15,7 +15,7 @@ public class VendingMachine {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("");
     }
     
 }
