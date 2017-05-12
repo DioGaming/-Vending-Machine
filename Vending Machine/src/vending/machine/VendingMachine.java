aebbdf91234bb817127cfb045fdf0eaff29d77c3
comichero11 <richard.lam.0301@gmail.com>
2017-05-12 11:05:53 -0700

    Add files via upload

--- Vending Machine/src/vending/machine/VendingMachine.java
+++ Vending Machine/src/vending/machine/VendingMachine.java
@@ -1,21 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package vending.machine;
 
 /**
  *
  * @author Richard Lam
  */
+import java.util.Scanner;
 public class VendingMachine {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        System.out.println("");
+        Scanner reader = new Scanner(System.in);
+         
+        while (true) {
+        System.out.println("Welcome to the Spirits Machine.");
+        System.out.println("The selection of drinks\n=======================");
+        System.out.println("1. Dreadrum\n2. Fetching Fizz\n3. Werewhiskey\n0. Quit the program");
+        System.out.print("Please enter your selection by entering the number: ");
+        
+        //drink selection I guess for the lols
+        int selection = Integer.parseInt(reader.nextLine());
+        
+        if (selection == 0) {
+            break;
+        } else if (selection == 1) {
+            
+        } else if (selection == 2) {
+            
+        } else if (selection == 3) {
+            
+        } else {
+            
+        }
+        }
     }
     
 }
