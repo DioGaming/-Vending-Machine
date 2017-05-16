4cc328d2421a3331f2f0564484ff0a272e0ab137
DioGaming <yashbulsara@gmail.com>
2017-05-16 11:10:59 -0700

    changecalc Added

---
+++ Vending Machine/src/changecalc.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * @author http://www.twitch.tv/diomondplaysgames
+ */
+import java.util.Scanner;
+import java.lang.Math;
+public class changecalc {
+    public static void main(String [] args) {
+        Scanner input = new Scanner(System.in);
+    }
+}
