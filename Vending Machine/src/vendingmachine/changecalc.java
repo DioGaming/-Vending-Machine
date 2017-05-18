23222b2e743f04434c8bc147d8bdd3f43a8e853d
DioGaming <yashbulsara@gmail.com>
2017-05-18 14:28:32 -0700

    Updated GUI changecalc import added

---
+++ Vending Machine/src/vendingmachine/changecalc.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vendingmachine;
+
+/*
+ * @author http://www.twitch.tv/diomondplaysgames
+ */
+import java.util.Scanner;
+import java.lang.Math;
+public class changecalc {
+    public static void main(String [] args){
+    Scanner input = new Scanner(System.in);
+    
+}
+}
