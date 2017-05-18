bf656ecac6afb1008a45fa362eddca6f5ab3e090
comichero11 <richard.lam.0301@gmail.com>
2017-05-18 14:29:31 -0700

    Add files via upload

--- Vending Machine/src/vending/machine/VendingMachine.java
+++ Vending Machine/src/vending/machine/VendingMachine.java
@@ -6,31 +6,12 @@ package vending.machine;
  * @author Richard Lam
  */
 import java.util.Scanner;
+import java.lang.Math;
 public class VendingMachine {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         
-        while (true) {
-        System.out.println("Welcome to the Spirits Machine.");
-        System.out.println("The selection of drinks\n=======================");
-        System.out.println("1. Dreadrum\n2. Fetching Fizz\n3. Werewhiskey\n0. Quit the program");
-        System.out.print("Please enter your selection by entering the number: ");
         
-        //drink selection I guess for the lols
-        int selection = Integer.parseInt(sc.nextLine());
-        
-        if (selection == 0) {
-            break;
-        } else if (selection == 1) {
-            
-        } else if (selection == 2) {
-            
-        } else if (selection == 3) {
-            
-        } else {
-            
-        }
-        }
     }
     
 }
