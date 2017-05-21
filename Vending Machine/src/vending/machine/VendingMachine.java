c7b476e36eb2bc7061a1929b4e0f7ea27dc230a8
mehtab99 <mehtab.chima@gmail.com>
2017-05-20 17:41:19 -0700

    Add files via upload

--- Vending Machine/src/vending/machine/VendingMachine.java
+++ Vending Machine/src/vending/machine/VendingMachine.java
@@ -1,10 +1,24 @@
 package vending.machine;
+
+import java.util.Scanner;
 /*
- * @author http://www.twitch.tv/diomondplaysgames
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Mehtab
  */
 public class VendingMachine {
+
+    Scanner scan = new Scanner(System.in);
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        
+        // TODO code application logic here
     }
     
 }
